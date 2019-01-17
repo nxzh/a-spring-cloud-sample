c2542b6aad14c42974913b8e90681dc7b8e3bdcc
sid.a.zhang <sid.a.zhang@capgemini.com>
2019-01-17 10:14:21 +0800

    add discovery service

---
+++ discovery-service/src/main/java/fun/nxzh/guilin/discovery/config/SecurityConfiguration.java
@@ -0,0 +1,102 @@
+package fun.nxzh.guilin.discovery.config;
+
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.BeanInitializationException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@Import(SecurityProblemSupport.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+  private final SecurityProblemSupport problemSupport;
+
+  public SecurityConfiguration(
+      AuthenticationManagerBuilder authenticationManagerBuilder,
+      SecurityProblemSupport problemSupport) {
+    this.authenticationManagerBuilder = authenticationManagerBuilder;
+    this.problemSupport = problemSupport;
+  }
+
+  @PostConstruct
+  public void init() {
+    try {
+      authenticationManagerBuilder
+          .inMemoryAuthentication()
+          .passwordEncoder(passwordEncoder())
+          .withUser("admin")
+          .password(passwordEncoder().encode("admin123"))
+          .roles("ADMIN");
+    } catch (Exception e) {
+      throw new BeanInitializationException("Security configuration failed", e);
+    }
+  }
+
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+        .antMatchers(HttpMethod.OPTIONS, "/**")
+        .antMatchers("/app/**/*.{js,html}")
+        .antMatchers("/i18n/**")
+        .antMatchers("/content/**")
+        .antMatchers("/swagger-ui/index.html")
+        .antMatchers("/test/**")
+        .antMatchers("/h2-console/**");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.exceptionHandling()
+        .authenticationEntryPoint(problemSupport)
+        .accessDeniedHandler(problemSupport)
+        .and()
+        .csrf()
+        .disable()
+        .headers()
+        .frameOptions()
+        .disable()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers("/eureka/**")
+        .permitAll()
+        .antMatchers("/management/health")
+        .permitAll()
+        .antMatchers("/management/info")
+        .permitAll()
+        .antMatchers("/management/**")
+        .hasAuthority(AuthoritiesConstants.ADMIN)
+        .and()
+        .httpBasic();
+  }
+}

9b4556d6218c72a7b41ecd06766afccd2d72fc8b
sid.a.zhang <sid.a.zhang@capgemini.com>
2019-01-24 18:11:12 +0800

    fix the eureka server.

--- discovery-service/src/main/java/fun/nxzh/guilin/discovery/config/SecurityConfiguration.java
+++ discovery-service/src/main/java/fun/nxzh/guilin/discovery/config/SecurityConfiguration.java
@@ -1,64 +1,15 @@
 package fun.nxzh.guilin.discovery.config;
 
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@Import(SecurityProblemSupport.class)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-  private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
-  private final SecurityProblemSupport problemSupport;
-
-  public SecurityConfiguration(
-      AuthenticationManagerBuilder authenticationManagerBuilder,
-      SecurityProblemSupport problemSupport) {
-    this.authenticationManagerBuilder = authenticationManagerBuilder;
-    this.problemSupport = problemSupport;
-  }
-
-  @PostConstruct
-  public void init() {
-    try {
-      authenticationManagerBuilder
-          .inMemoryAuthentication()
-          .passwordEncoder(passwordEncoder())
-          .withUser("system")
-          .password(passwordEncoder().encode("sys123"))
-          .roles("SYSTEM");
-    } catch (Exception e) {
-      throw new BeanInitializationException("Security configuration failed", e);
-    }
-  }
-
-  @Override
-  @Bean
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
 
   @Override
   public void configure(WebSecurity web) throws Exception {
@@ -74,25 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.exceptionHandling()
-        .authenticationEntryPoint(problemSupport)
-        .accessDeniedHandler(problemSupport)
-        .and()
-        .csrf()
-        .disable()
-        .headers()
-        .frameOptions()
-        .disable()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authorizeRequests()
-        .antMatchers("/eureka/**")
-        .permitAll()
-        .antMatchers("/manage/**")
-        .hasAuthority(AuthoritiesConstants.SYSTEM)
-        .and()
-        .httpBasic();
+    http.csrf().ignoringAntMatchers("/eureka/**");
+    super.configure(http);
   }
 }

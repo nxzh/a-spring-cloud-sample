c8122821b27535024faf97ae4dc1bc52fb6e2a15
Sid Zhang <zhangnaixiao@outlook.com>
2019-01-28 21:18:04 +0800

    removed unused configuration

--- infra-discovery-service/src/main/java/fun/nxzh/guilin/discovery/config/SecurityConfiguration.java
+++ infra-discovery-service/src/main/java/fun/nxzh/guilin/discovery/config/SecurityConfiguration.java
@@ -1,27 +1,13 @@
 package fun.nxzh.guilin.discovery.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web.ignoring()
-        .antMatchers(HttpMethod.OPTIONS, "/**")
-        .antMatchers("/app/**/*.{js,html}")
-        .antMatchers("/i18n/**")
-        .antMatchers("/content/**")
-        .antMatchers("/swagger-ui/index.html")
-        .antMatchers("/test/**")
-        .antMatchers("/h2-console/**");
-  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {

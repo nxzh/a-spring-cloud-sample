4870f121c63ce23c36473f7f12e344c92c392221
Sid Zhang <zhangnaixiao@outlook.com>
2019-02-03 21:20:44 +0800

    add gateway

---
+++ biz-facade-service/src/main/java/fun/nxzh/guilin/facade/WebSecurityConfiguration.java
@@ -0,0 +1,14 @@
+package fun.nxzh.guilin.facade;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/**");
+  }
+}

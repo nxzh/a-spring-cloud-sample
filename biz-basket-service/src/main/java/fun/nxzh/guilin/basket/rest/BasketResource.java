df9f2364f5028bd0ba72119edd4527815f0c6673
Sid Zhang <zhangnaixiao@outlook.com>
2019-02-06 23:00:22 +0800

    add oauth2 config with token relay

--- biz-basket-service/src/main/java/fun/nxzh/guilin/basket/rest/BasketResource.java
+++ biz-basket-service/src/main/java/fun/nxzh/guilin/basket/rest/BasketResource.java
@@ -3,7 +3,6 @@ package fun.nxzh.guilin.basket.rest;
 import fun.nxzh.guilin.basket.model.Basket;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

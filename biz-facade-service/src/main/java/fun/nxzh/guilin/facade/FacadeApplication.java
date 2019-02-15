94a0983e37952c49740aa6b5dea59b2aca790b64
sid.a.zhang <sid.a.zhang@capgemini.com>
2019-02-15 18:16:42 +0800

    add hystrix aggregate.

--- biz-facade-service/src/main/java/fun/nxzh/guilin/facade/FacadeApplication.java
+++ biz-facade-service/src/main/java/fun/nxzh/guilin/facade/FacadeApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @EnableConfigurationProperties(ApplicationProperties.class)
 public class FacadeApplication {
 
---
+++ infra-turbine-service/src/main/java/fun/nxzh/guilin/turbine/TurbineService.java
@@ -0,0 +1,18 @@
+package fun.nxzh.guilin.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableTurbineStream
+public class TurbineService {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TurbineService.class, args);
+  }
+}
